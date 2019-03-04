e059d614d1259a999649700e1550535878c62149
Tuan Dao <dmtuan@tma.com.vn>
2019-03-04 17:49:06 +0700

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrintManager
+{
+    public partial class FrmPrintManager : Form
+    {
+        public FrmPrintManager()
+        {
+            InitializeComponent();
+
+            this.Start();
+        }
+
+        public void Start()
+        {
+            PrintControl printControl = new PrintControl();
+            printControl.getPrinters();
+        }
+    }
+}
---
+++ PrintControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace PrintManager
+{
+    class PrintControl
+    {
+        public PrintControl()
+        {
+            Console.WriteLine("Start Print-Control");
+        }
+
+        public void getPrinters()
+        {
+            var printerQuery = new ManagementObjectSearcher("SELECT * from Win32_Printer");
+            foreach (var printer in printerQuery.Get())
+            {
+                var name = printer.GetPropertyValue("Name");
+                var status = printer.GetPropertyValue("Status");
+                var isDefault = printer.GetPropertyValue("Default");
+                var isNetworkPrinter = printer.GetPropertyValue("Network");
+
+                Console.WriteLine("{0} (Status: {1}, Default: {2}, Network: {3}",
+                            name, status, isDefault, isNetworkPrinter);
+            }
+        }
+    }
+}
