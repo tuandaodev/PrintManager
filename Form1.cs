eda374a9535bbc208479a21eaf266edbf74017d7
tuandaodev <tuandao.dev@gmail.com>
2019-03-11 04:47:24 +0700

    up

--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -21,8 +24,42 @@ namespace PrintManager
 
         public void Start()
         {
-            PrintControl printControl = new PrintControl();
-            printControl.getPrinters();
+
+            var data = GET("http://localhost/khos2/public/index.php/api/print-danh-sach-don-hang");
+
+           // object yourOjbect = JsonConvert.DeserializeObject(data);
+
+            object yourOjbect = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic[]>(data);
+
+            Console.WriteLine("DONE");
+
+            //PrintControl printControl = new PrintControl();
+            //printControl.getPrinters();
+        }
+
+        public string GET(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                    // log errorText
+                }
+                throw;
+            }
         }
     }
 }
--- PrintControl.cs
+++ PrintControl.cs
@@ -16,17 +16,17 @@ namespace PrintManager
 
         public void getPrinters()
         {
-            var printerQuery = new ManagementObjectSearcher("SELECT * from Win32_Printer");
-            foreach (var printer in printerQuery.Get())
-            {
-                var name = printer.GetPropertyValue("Name");
-                var status = printer.GetPropertyValue("Status");
-                var isDefault = printer.GetPropertyValue("Default");
-                var isNetworkPrinter = printer.GetPropertyValue("Network");
+            //var printerQuery = new ManagementObjectSearcher("SELECT * from Win32_Printer");
+            //foreach (var printer in printerQuery.Get())
+            //{
+            //    var name = printer.GetPropertyValue("Name");
+            //    var status = printer.GetPropertyValue("Status");
+            //    var isDefault = printer.GetPropertyValue("Default");
+            //    var isNetworkPrinter = printer.GetPropertyValue("Network");
 
-                Console.WriteLine("{0} (Status: {1}, Default: {2}, Network: {3}",
-                            name, status, isDefault, isNetworkPrinter);
-            }
+            //    Console.WriteLine("{0} (Status: {1}, Default: {2}, Network: {3}",
+            //                name, status, isDefault, isNetworkPrinter);
+            //}
         }
     }
 }
